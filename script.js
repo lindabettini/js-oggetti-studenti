878e0fb0e42c081148f71c5deb15cd0025de585b
lindabettini <linda.bettini@hotmail.it>
2022-01-19 17:16:03 +0100

    Add array studentList

--- script.js
+++ script.js
@@ -1,13 +1,28 @@
 const studenteElement = document.getElementById('studente');
 
-const studente = {
+const studenteUno = {
     nome: 'Linda',
     cognome: 'Bettini',
     eta: 27, 
 }
 
-for (let key in studente) {
-    console.log(`${key} : ${studente[key]}`);
-    let studToPrint = (`${key} : ${studente[key]}`);
+for (let key in studenteUno) {
+    console.log(`${key} : ${studenteUno[key]}`);
+    let studToPrint = (`${key} : ${studenteUno[key]}`);
     studenteElement.innerHTML += `<div>${studToPrint}</div>`;
 }
+
+const studenteDue = {
+    nome: 'Lucia',
+    cognome: 'Dragoni',
+    eta: 27, 
+}
+
+const studenteTre = {
+    nome: 'Martina',
+    cognome: 'Bucciarelli',
+    eta: 27, 
+}
+
+const studentList = [studenteUno, studenteDue, studenteTre]; 
+console.log(studentList);
