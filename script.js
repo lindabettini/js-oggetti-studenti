ba7325c6c7d388562de3a25b042e53a02eceeeec
lindabettini <linda.bettini@hotmail.it>
2022-01-19 16:51:52 +0100

    print attempt

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const studenteElement = document.getElementById('studente');
+
 const studente = {
     nome: 'Linda',
     cognome: 'Bettini',
@@ -6,4 +8,5 @@ const studente = {
 
 for (let key in studente) {
     console.log(`${key} : ${studente[key]}`);
+    studenteElement.innerHTML = (`${key} : ${studente[key]}`);
 }
